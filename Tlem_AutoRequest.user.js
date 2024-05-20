87fb171fc17258d3cab74e788511fb386bee2ab6
Rutra09 <amzadrozny@gmail.com>
2024-05-20 17:23:29 +0200

    Beta Version

--- Tlem_AutoRequest.user.js
+++ Tlem_AutoRequest.user.js
@@ -1,60 +1,28 @@
 // ==UserScript==
 // @name         Tlem AutoRequest
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1b
 // @description  try to take over the world!
 // @author       ArturM
 // @match        https://edu.t-lem.com/
-// @updateURL    https://github.com/Rutra09/tlem_autoprompt/raw/master/Tlem_AutoPrompt2.user.js
-// @downloadURL  https://github.com/Rutra09/tlem_autoprompt/raw/master/Tlem_AutoPrompt2.user.js
+// @updateURL    https://raw.githubusercontent.com/Rutra09/tlem_autorequest/main/Tlem_AutoRequest.user.js
+// @downloadURL  https://raw.githubusercontent.com/Rutra09/tlem_autorequest/main/Tlem_AutoRequest.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=t-lem.com
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://raw.githubusercontent.com/Rutra09/tlem_autorequest/main/Utility.user.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_cookie
 // ==/UserScript==
 
-function getAnswer(id) {
-    return new Promise((resolve, reject) => {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: `http://tlem.arturm.me/getLesson?id=${id}`,
-            onload: function (response) {
-                resolve(response);
-            }
-        })
-    })
-}
 
-function getLessonId() {
-    return window.location.href.split(":")[2];
-}
-
-function getFileName() {
-    let el = document.querySelectorAll("[id^=check_file_][id$=_cpp]")[0];
-    return el.id.split("_")[2] + ".cpp";
-}
-
-function getFilesNames() {
-    let els = document.querySelectorAll("[id^=check_file_][id$=_cpp]");
-    let names = [];
-    for (let i = 0; i < els.length; i++) {
-        names.push(els[i].id.split("_")[2] + ".cpp");
-    }
-    return names;
-}
-
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function insertCodeSQL(code, file) {
+function insertCodeSQL(code) {
     waitForKeyElements("#sql_cmd", function () {
         setTimeout(() => {
             console.log(code);
             ace.edit("sql_cmd").setValue(code);
             ace.edit("sql_cmd").clearSelection();
-            handlers.sql.process()   
-            goToNextLesson();    
+            console.log(handlers.sql.cmd, handlers.lekcja)
+            handlers.sql.process();
         }, 250);
     }, true);
 }
@@ -72,21 +40,36 @@ function submitAnswer() {
     });
 }
 
-function goToNextLesson() {
-    let lekcjaNextButton = document.getElementById("lekcja_next");
-    if (lekcjaNextButton == null) {
-        return;
-    }
-    lekcjaNextButton.click();
-    setTimeout(() =>  {
-        submitAnswer()
-        console.log("Next lesson");
-    }, 500);
-    
+function handleSQL() {
+    let id = getLessonId();
+    getAnswer(id).then((response) => {
+        if(response.responseText == "No data for this lesson") {
+            alert("W bazie nie ma jeszcze tego zadania. \nSkontatkuj się z Arturkiem albo Gąską. (Ewentualnie zadzwoń do J.P.)")
+            return;
+        }
+        let data = JSON.parse(response.responseText);
+        let code = data["plik.cpp"]
+        insertCodeSQL(code);
+        goToNextLesson();
+    });
 }
 
+
 function lessonDetector() {
-    waitForKeyElements("#lekcja_next", () => submitAnswer(), true);
+    waitForKeyElements("#lekcja-items", () => {
+        determineLessonTypes().then((lessonTypes) => {
+            switch (lessonTypes[0]) {
+                case "code":
+                    break;
+                case "sql":
+
+                    submitAnswer();
+                    break;
+                default:
+                    alert("Nieznany typ lekcji");
+            }
+        });
+    });
 }
 
 function init() {
---
+++ Utility.user.js
@@ -0,0 +1,53 @@
+function getFileName() {
+    let el = document.querySelectorAll("[id^=check_file_][id$=_cpp]")[0];
+    return el.id.split("_")[2] + ".cpp";
+}
+
+function getFilesNames() {
+    let els = document.querySelectorAll("[id^=check_file_][id$=_cpp]");
+    let names = [];
+    for (let i = 0; i < els.length; i++) {
+        names.push(els[i].id.split("_")[2] + ".cpp");
+    }
+    return names;
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function goToNextLesson() {
+    let lekcjaNextButton = document.getElementById("lekcja_next");
+    if (lekcjaNextButton == null) {
+        return;
+    }
+    lekcjaNextButton.click();
+}
+
+function getLessonId() {
+    return window.location.href.split(":")[2];
+}
+
+function determineLessonTypes() {
+    return new Promise((resolve, reject) => {
+        let lessonTypes = []
+        let activeLekcja = document.querySelectorAll("#lekcja-items > *.active");
+        activeLekcja.forEach(function (element) {
+            let lessonType = element.id.split("-")[1].slice("t_".length);
+            lessonTypes.push(lessonType);
+        });
+        resolve(lessonTypes);
+    });
+}
+
+function getAnswer(id) {
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: `http://tlem.arturm.me/getLesson?id=${id}`,
+            onload: function (response) {
+                resolve(response);
+            }
+        })
+    })
+}
